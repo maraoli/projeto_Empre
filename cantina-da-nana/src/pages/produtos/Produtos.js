947533b8283344d485e32153a20b3245c75108e2
Mara <mara.15oliveira@outlook.com>
2020-11-19 21:48:31 -0300

    ajustes

--- cantina-da-nana/src/pages/produtos/Produtos.js
+++ cantina-da-nana/src/pages/produtos/Produtos.js
@@ -1,10 +1,126 @@
 import React from 'react';
 import PageDefault from '../../components/pageDefault/PageDefault'
+import ImgDoces from '../../assets/doces-1.jpeg';
+import {FaArrowRight} from 'react-icons/fa'
+import './Produtos.css'
 
 export default function Produtos(){
     return(
         <PageDefault>
-            <h1>Produtos</h1>
+            <div className="produtos__container">
+                <FaArrowRight className="produtos__arrow"/>
+                <input id="produtos-open__aside" type="checkbox"/>
+                <aside className="produtos__lista">
+                    <ul>
+                        <li>Salgados</li>
+                        <li>Doces</li>
+                        <li>Bebidas</li>
+                        <li>Bolos</li>
+                        <li>Tortas</li>
+                        <li>Pizzas</li>
+                    </ul>
+                </aside>
+                <div className="produtos__produtos">
+                    <div className="produtos__produto">
+                        <img src={ImgDoces} alt="Doce" />
+                        <div>
+                            <p>Docinhos Variados</p>
+                            <p>R$: 10,00</p>
+                        </div>
+                    </div>
+                    <div className="produtos__produto">
+                        <img src={ImgDoces} alt="Doce" />
+                        <div>
+                            <p>Docinhos Variados</p>
+                            <p>R$: 10,00</p>
+                        </div>
+                    </div>
+                    <div className="produtos__produto">
+                        <img src={ImgDoces} alt="Doce" />
+                        <div>
+                            <p>Docinhos Variados</p>
+                            <p>R$: 10,00</p>
+                        </div>
+                    </div>
+                    <div className="produtos__produto">
+                        <img src={ImgDoces} alt="Doce" />
+                        <div>
+                            <p>Docinhos Variados</p>
+                            <p>R$: 10,00</p>
+                        </div>
+                    </div>
+                    <div className="produtos__produto">
+                        <img src={ImgDoces} alt="Doce" />
+                        <div>
+                            <p>Docinhos Variados</p>
+                            <p>R$: 10,00</p>
+                        </div>
+                    </div>
+                    <div className="produtos__produto">
+                        <img src={ImgDoces} alt="Doce" />
+                        <div>
+                            <p>Docinhos Variados</p>
+                            <p>R$: 10,00</p>
+                        </div>
+                    </div>
+                    <div className="produtos__produto">
+                        <img src={ImgDoces} alt="Doce" />
+                        <div>
+                            <p>Docinhos Variados</p>
+                            <p>R$: 10,00</p>
+                        </div>
+                    </div>
+                    <div className="produtos__produto">
+                        <img src={ImgDoces} alt="Doce" />
+                        <div>
+                            <p>Docinhos Variados</p>
+                            <p>R$: 10,00</p>
+                        </div>
+                    </div>
+                    <div className="produtos__produto">
+                        <img src={ImgDoces} alt="Doce" />
+                        <div>
+                            <p>Docinhos Variados</p>
+                            <p>R$: 10,00</p>
+                        </div>
+                    </div>
+                    <div className="produtos__produto">
+                        <img src={ImgDoces} alt="Doce" />
+                        <div>
+                            <p>Docinhos Variados</p>
+                            <p>R$: 10,00</p>
+                        </div>
+                    </div>
+                    <div className="produtos__produto">
+                        <img src={ImgDoces} alt="Doce" />
+                        <div>
+                            <p>Docinhos Variados</p>
+                            <p>R$: 10,00</p>
+                        </div>
+                    </div>
+                    <div className="produtos__produto">
+                        <img src={ImgDoces} alt="Doce" />
+                        <div>
+                            <p>Docinhos Variados</p>
+                            <p>R$: 10,00</p>
+                        </div>
+                    </div>
+                    <div className="produtos__produto">
+                        <img src={ImgDoces} alt="Doce" />
+                        <div>
+                            <p>Docinhos Variados</p>
+                            <p>R$: 10,00</p>
+                        </div>
+                    </div>
+                    <div className="produtos__produto">
+                        <img src={ImgDoces} alt="Doce" />
+                        <div>
+                            <p>Docinhos Variados</p>
+                            <p>R$: 10,00</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </PageDefault>
     )
 }
